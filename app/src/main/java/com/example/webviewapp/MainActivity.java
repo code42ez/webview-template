7671431716938c0463dd12f94be7c6054621bfe7
code42ez <snek87150@gmail.com>
2025-05-31 19:29:25 +0200

    Create MainActivity.java

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        webView = new WebView(this);
+        setContentView(webView);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://example.com"); // À remplacer plus tard par l'URL du site de l'utilisateur
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
